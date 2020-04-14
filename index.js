a2196fb4ff2fa4489d1b0edf4d4edb3d78062cc4
trycrmr <trycrmr@gmail.com>
2020-04-14 16:36:17 -0400

    Enables fetching gzipped dataset

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => res.sendFile(`${__dirname}/index.html`))
-app.get('/data.json', (req, res) => res.sendFile(`${__dirname}/data.json`))
+app.get('/data.json.gz', (req, res) => {
+  res.setHeader('Content-Encoding', 'gzip')
+  res.sendFile(`${__dirname}/data.json.gz`)
+})
 
 app.listen(port, () => console.log(`coviddata.website server running on port ${port}; Give EVERYONE EVERYWHERE insight into the latest COVID-19 data.`))
