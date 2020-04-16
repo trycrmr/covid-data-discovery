81dd59699f4fb72662b447234d5cccac8a07a73e
trycrmr <trycrmr@gmail.com>
2020-04-16 19:22:46 -0400

    Fixes some integration lambda@edge issues

--- deploy/aws/addHeader.js
+++ deploy/aws/addHeader.js
@@ -1,22 +1,19 @@
-'use strict';
-
-exports.handler = (event, context, callback) => {
+exports.handler = async (event, context) => {
   const requestURI = event.Records[0].cf.request.uri
-  if(requestURI === '/index.html') callback(null, response);
   const response = event.Records[0].cf.response
 
   const headerName = 'File-Size'
-  const UNCOMPRESSED_DATA_SIZE = 14749554
-  const UNCOMPRESSED_PLOTLY_SIZE = 3325432
+  const UNCOMPRESSED_DATA_SIZE = '14749554'
+  const UNCOMPRESSED_PLOTLY_SIZE = '3325432'
 
   switch(requestURI) {
     case '/data.json.gz':
       response.headers[headerName] = [{ key: headerName, value: UNCOMPRESSED_DATA_SIZE }];
-      callback(null, response);
-      break
+      return response
     case '/plotly-1.53.0.min.js.gz':
       response.headers[headerName] = [{ key: headerName, value: UNCOMPRESSED_PLOTLY_SIZE }];
-      callback(null, response);
-      break
+      return response
+    default: 
+      return response
   }
 }
