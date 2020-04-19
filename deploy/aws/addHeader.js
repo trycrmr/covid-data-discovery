a83912927cf232302a6dc4ee04b83f168caf1e7b
trycrmr <trycrmr@gmail.com>
2020-04-19 15:58:15 -0400

    Sets data size for 4/18 load

--- deploy/aws/addHeader.js
+++ deploy/aws/addHeader.js
@@ -3,7 +3,7 @@ exports.handler = async (event, context) => {
   const response = event.Records[0].cf.response
 
   const headerName = 'File-Size'
-  const UNCOMPRESSED_DATA_SIZE = '14749554'
+  const UNCOMPRESSED_DATA_SIZE = '14766812'
   const UNCOMPRESSED_PLOTLY_SIZE = '3325432'
 
   switch(requestURI) {
