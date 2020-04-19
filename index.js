ddfce8b8731ac45d684a89f9d9cf7d60eb4da613
trycrmr <trycrmr@gmail.com>
2020-04-19 14:43:00 -0400

    Sets headers in dev server same as canary set them

--- index.js
+++ index.js
@@ -9,12 +9,14 @@ app.get('/favicon.ico', (req, res) => res.sendFile(`${__dirname}/favicon.ico`))
 app.get('/data.json.gz', (req, res) => {
   res.setHeader('Content-Encoding', 'gzip')
   res.setHeader('Transfer-Encoding', 'gzip')
+  res.setHeader('Content-Type', 'application/json')
   res.setHeader('File-Size', UNCOMPRESSED_DATA_SIZE) // Non-standard header; Used for calculating download progress bar on the front-end.
   res.sendFile(`${__dirname}/data.json.gz`)
 })
 app.get('/plotly-1.53.0.min.js.gz', (req, res) => { // Freezing plotly version so the dependency doesn't change underfoot and this app can control the headers
   res.setHeader('Content-Encoding', 'gzip')
   res.setHeader('Transfer-Encoding', 'gzip')
+  res.setHeader('Content-Type', 'application/javascript')
   res.setHeader('File-Size', UNCOMPRESSED_PLOTLY_SIZE) // Non-standard header; Used for calculating download progress bar on the front-end.
   res.sendFile(`${__dirname}/plotly-1.53.0.min.js.gz`)
 })
