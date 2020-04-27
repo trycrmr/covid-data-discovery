c19c2a4e22b3a1e567fa92cf24df5a65997f437b
trycrmr <trycrmr@gmail.com>
2020-04-27 09:06:49 -0400

    Update dev env w/ JHU data 4/26

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 3000
-const UNCOMPRESSED_DATA_SIZE = 14850466
+const UNCOMPRESSED_DATA_SIZE = 14856889
 const UNCOMPRESSED_PLOTLY_SIZE = 3325432
 
 app.get('/', (req, res) => res.sendFile(`${__dirname}/index.html`))
