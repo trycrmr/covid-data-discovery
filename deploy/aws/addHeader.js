bcff72a5837e828633bb4d92d6ddf0dd6f4876e7
trycrmr <trycrmr@gmail.com>
2020-06-19 08:35:47 -0400

    JHU data upload 6/18

--- deploy/aws/addHeader.js
+++ deploy/aws/addHeader.js
@@ -3,7 +3,7 @@ exports.handler = async (event, context) => {
   const response = event.Records[0].cf.response
 
   const headerName = 'File-Size'
-  const UNCOMPRESSED_DATA_SIZE = '121631961'
+  const UNCOMPRESSED_DATA_SIZE = '122458926'
   const UNCOMPRESSED_PLOTLY_SIZE = '3325432'
 
   switch(requestURI) {
