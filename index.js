bf09dca6bbfd53032091e004fa8edeb3db9c3a0b
trycrmr <trycrmr@gmail.com>
2020-07-03 15:38:13 -0400

    JHU data load 7/2

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 3000
-const UNCOMPRESSED_DATA_SIZE = 133221848
+const UNCOMPRESSED_DATA_SIZE = 134051125
 const UNCOMPRESSED_PLOTLY_SIZE = 3325432
 
 app.get('/', (req, res) => res.sendFile(`${__dirname}/index.html`))
