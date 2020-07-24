94b0254b1a248df172751e6ddd7fac86c56c1378
trycrmr <trycrmr@gmail.com>
2020-07-24 11:21:45 -0400

    JHU data load 7/23

--- deploy/aws/addHeader.js
+++ deploy/aws/addHeader.js
@@ -1,23 +1,35 @@
 exports.handler = async (event, context) => {
-  const requestURI = event.Records[0].cf.request.uri
-  const response = event.Records[0].cf.response
+  const requestURI = event.Records[0].cf.request.uri;
+  const response = event.Records[0].cf.response;
 
-  const headerName = 'File-Size'
-  const UNCOMPRESSED_DATA_SIZE = '144010377'
-  const UNCOMPRESSED_PLOTLY_SIZE = '3325432'
+  const headerName = "File-Size";
+  const UNCOMPRESSED_DATA_SIZE = "153567137";
+  const UNCOMPRESSED_PLOTLY_SIZE = "3325432";
 
-  switch(requestURI) {
-    case '/data.json.gz':
-      response.headers['File-Size'] = [{ key: 'File-Size', value: UNCOMPRESSED_DATA_SIZE }];
-      response.headers['Content-Encoding'] = [{ key: 'Content-Encoding', value: 'gzip' }];
-      response.headers['Content-Type'] = [{ key: 'Content-Type', value: 'application/json' }];
-      return response
-    case '/plotly-1.53.0.min.js.gz':
-      response.headers['File-Size'] = [{ key: headerName, value: UNCOMPRESSED_PLOTLY_SIZE }];
-      response.headers['Content-Encoding'] = [{ key: 'Content-Encoding', value: 'gzip' }];
-      response.headers['Content-Type'] = [{ key: 'Content-Type', value: 'application/javascript' }];
-      return response
-    default: 
-      return response
+  switch (requestURI) {
+    case "/data.json.gz":
+      response.headers["File-Size"] = [
+        { key: "File-Size", value: UNCOMPRESSED_DATA_SIZE },
+      ];
+      response.headers["Content-Encoding"] = [
+        { key: "Content-Encoding", value: "gzip" },
+      ];
+      response.headers["Content-Type"] = [
+        { key: "Content-Type", value: "application/json" },
+      ];
+      return response;
+    case "/plotly-1.53.0.min.js.gz":
+      response.headers["File-Size"] = [
+        { key: headerName, value: UNCOMPRESSED_PLOTLY_SIZE },
+      ];
+      response.headers["Content-Encoding"] = [
+        { key: "Content-Encoding", value: "gzip" },
+      ];
+      response.headers["Content-Type"] = [
+        { key: "Content-Type", value: "application/javascript" },
+      ];
+      return response;
+    default:
+      return response;
   }
-}
+};
--- index.js
+++ index.js
@@ -1,24 +1,29 @@
-const express = require('express')
-const app = express()
-const port = 3000
-const UNCOMPRESSED_DATA_SIZE = 144010377
-const UNCOMPRESSED_PLOTLY_SIZE = 3325432
+const express = require("express");
+const app = express();
+const port = 3000;
+const UNCOMPRESSED_DATA_SIZE = 153567137;
+const UNCOMPRESSED_PLOTLY_SIZE = 3325432;
 
-app.get('/', (req, res) => res.sendFile(`${__dirname}/index.html`))
-app.get('/favicon.ico', (req, res) => res.sendFile(`${__dirname}/favicon.ico`))
-app.get('/data.json.gz', (req, res) => {
-  res.setHeader('Content-Encoding', 'gzip')
-  res.setHeader('Transfer-Encoding', 'gzip')
-  res.setHeader('Content-Type', 'application/json')
-  res.setHeader('File-Size', UNCOMPRESSED_DATA_SIZE) // Non-standard header; Used for calculating download progress bar on the front-end.
-  res.sendFile(`${__dirname}/data.json.gz`)
-})
-app.get('/plotly-1.53.0.min.js.gz', (req, res) => { // Freezing plotly version so the dependency doesn't change underfoot and this app can control the headers
-  res.setHeader('Content-Encoding', 'gzip')
-  res.setHeader('Transfer-Encoding', 'gzip')
-  res.setHeader('Content-Type', 'application/javascript')
-  res.setHeader('File-Size', UNCOMPRESSED_PLOTLY_SIZE) // Non-standard header; Used for calculating download progress bar on the front-end.
-  res.sendFile(`${__dirname}/plotly-1.53.0.min.js.gz`)
-})
+app.get("/", (req, res) => res.sendFile(`${__dirname}/index.html`));
+app.get("/favicon.ico", (req, res) => res.sendFile(`${__dirname}/favicon.ico`));
+app.get("/data.json.gz", (req, res) => {
+  res.setHeader("Content-Encoding", "gzip");
+  res.setHeader("Transfer-Encoding", "gzip");
+  res.setHeader("Content-Type", "application/json");
+  res.setHeader("File-Size", UNCOMPRESSED_DATA_SIZE); // Non-standard header; Used for calculating download progress bar on the front-end.
+  res.sendFile(`${__dirname}/data.json.gz`);
+});
+app.get("/plotly-1.53.0.min.js.gz", (req, res) => {
+  // Freezing plotly version so the dependency doesn't change underfoot and this app can control the headers
+  res.setHeader("Content-Encoding", "gzip");
+  res.setHeader("Transfer-Encoding", "gzip");
+  res.setHeader("Content-Type", "application/javascript");
+  res.setHeader("File-Size", UNCOMPRESSED_PLOTLY_SIZE); // Non-standard header; Used for calculating download progress bar on the front-end.
+  res.sendFile(`${__dirname}/plotly-1.53.0.min.js.gz`);
+});
 
-app.listen(port, () => console.log(`coviddata.website server running on port ${port}; Give EVERYONE EVERYWHERE insight into the latest COVID-19 data.`))
+app.listen(port, () =>
+  console.log(
+    `coviddata.website server running on port ${port}; Give EVERYONE EVERYWHERE insight into the latest COVID-19 data.`
+  )
+);
